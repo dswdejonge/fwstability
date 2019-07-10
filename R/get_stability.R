a335c94a798290c543a29dd22999580571bc84a4
dswdejonge <dsw.dejonge@gmail.com>
2019-07-10 17:30:52 +0200

    Added documentation for functions.

--- R/get_stability.R
+++ R/get_stability.R
@@ -1,3 +1,12 @@
+#' Get the mathematical stability of a matrix.
+#'
+#' This function allows you to find the stability of a state matrix by
+#' finding the maximum value of the real part of its eigenvalues.
+#' @param JM A Jacobian matrix.
+#' @return a numeric value, a negative value indicates a stable matrix.
+#' @export
+#' @examples
+#' getStability(JM)
 getStability <- function(JM) {
   stability <- max(Re(eigen(JM)$values))
   return(stability)
