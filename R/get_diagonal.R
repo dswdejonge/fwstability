9b702e346d7395b2f3ce01571a13c35ce8036774
dswdejonge <dsw.dejonge@gmail.com>
2019-08-02 18:32:08 +0200

    Updated diagonal for detritus so that it works with multiple compartments. Added wrapper function for both species and detritus diagonal values. Added corresponding tests.

--- R/get_diagonal.R
+++ R/get_diagonal.R
@@ -23,7 +23,12 @@ getDiagonalSpecies <- function(MR, BM) {
     stop("input vectors have unequal lengths")
   } else if(!is.numeric(MR) | !is.numeric(BM)) {
     stop("input vectors must be numeric")
+  } else if(is.null(names(MR)) | is.null(names(BM))) {
+    stop("input vectors must be named")
+  } else if(!all(names(MR) == names(BM))) {
+    stop("names of vectors do not match")
   }
+
   result <- -MR/BM
   #result <- -getMortalityRate(flow_solutions, BM, dead)
   #result <- result[-dead]
@@ -34,33 +39,86 @@ getDiagonalSpecies <- function(MR, BM) {
 #'
 #' This function finds diagonal values for detritus compartments to be used in the Jacobian
 #' matrix. The calculation is based on the total amount of assimilated detritus in all
-#' consumers of detritus and the biomass of the detritus compartment, and is an
+#' consumers of detritus and the biomass of the detritus compartments, and is an
 #' implementation of the equation from Neutel & Thorne (2014). \cr
 #' References: \cr
 #' Neutel, A.M., Thorne, M.A.S., 2014. Interaction strengths in balanced carbon cycles
 #' and the absence of a relation between ecosystem complexity and stability. Ecol. Lett. 17,
 #' 651–661. https://doi.org/10.1111/ele.12266
-#' @param CR A named numeric vector with detritus consumption rates of all compartments
-#' feeding on the detritus compartment. (required)
-#' @param AE A named numeric vector with assimilation efficiencies of all compartments
-#' feeding on the detritus compartment. Must be in the same order as CR. Must be a
-#' fraction i.e. between 0 and 1 (required)
-#' @param BM The biomass of the detritus compartment.
-#' @return This function returns a value which is the diagonal value for the
-#' detritus compartment in the food web (per unit time). It is important to review the units of the
-#' input data. If CR is biomass per unit time then BM must be just biomass. If CR is
+#' @param FM A named square flowmatrix, source compartments as rows,
+#' sink compartments as columns. Should NOT contain external compartments. (required)
+#' @param BM A named numeric vector with biomasses of all compartments, must be in the same
+#' order as the flow matrix after externals are excluded. (required)
+#' @param AE A named numeric vector with assimilation efficiencies of all
+#' compartments, must be in the same order as the flow matrix after externals
+#' are excluded. Must be a fraction i.e. between 0 and 1 (required)
+#' @param dead Character vector with all names of detritus and nutrient
+#' compartments (everything that is not fauna). (required)
+#' @return This function returns a named numeric vector with the diagonal values for the
+#' detritus compartments in the food web (per unit time). It is important to review the units of the
+#' input data. If the FM is biomass per unit time then BM must be just biomass. If FM is
 #' biomass per unit time per surface area then BM must be biomass per surface area.
-getDiagonalDetritus <- function(CR, BM, AE){
-  result <- -sum(AE * CR) / BM
-  #detritus_ingestion <- colSums(t(FM[dead,-dead])*AE[-dead])
-  #result <- -detritus_ingestion/BM[dead]
+getDiagonalDetritus <- function(FM, BM, AE, dead){
+  dead_i <- which(rownames(FM) %in% dead)
+  if(length(dead) == 1) {
+    det_assimilation <- sum(t(FM[dead_i, -dead_i] * AE[-dead_i]))
+  } else {
+    det_assimilation <- colSums(t(FM[dead_i, -dead_i] * AE[-dead_i]))
+  }
+  result <- -det_assimilation/BM[dead_i]
   return(result)
 }
 
-#diagonal <- diag(FM)
-#aii <- diagonalSpecies(flow_solutions = pars$X, BM, dead)
-#add <- diagonalDetritus(FM[-externals, -externals], BM, AE, dead)
-#diagonal[names(aii)] <- aii
-#diagonal[names(add)] <- add
-#diag(JM2) <- diagonal[-externals]
-#JM2[is.na(JM2)] <- 0
+
+#' Calculate Jacobian diagonal from flux values
+#'
+#' This function finds diagonal values to be used in the Jacobian matrix based on flux
+#' values. Diagonal values for species depend on their non-predatory mortality rate and
+#' biomass. Diagonal values for dead (detritus) compartments are based on the total amount
+#' of assimilated detritus in all consumers of detritus and the biomass of the detritus
+#' compartments. This function implements the equations from from Neutel & Thorne (2014).
+#' The input should be named according to the food web compartment names. \cr
+#' References: \cr
+#' Neutel, A.M., Thorne, M.A.S., 2014. Interaction strengths in balanced carbon cycles
+#' and the absence of a relation between ecosystem complexity and stability. Ecol. Lett. 17,
+#' 651–661. https://doi.org/10.1111/ele.12266
+#' @param MR A named numeric vector with non-predatory mortality rates for all
+#' compartments (biomass per unit time or biomass per unit time per surface area).
+#' (required)
+#' @param BM A named numeric vector with biomasses of all compartments
+#' (just biomass or biomass per unit area), must be in the same order as MR. (required)
+#' @param dead Character vector with all names of detritus and nutrient
+#' compartments (everything that is not fauna). (optional)
+#' @param FM A named square flowmatrix, source compartments as rows,
+#' sink compartments as columns. Should NOT contain external compartments. Order of names
+#' must be equal to MR. (required when dead compartments are included)
+#' @param AE A named numeric vector with assimilation efficiencies of all
+#' compartments, must be in the same order as MR. Must be a fraction i.e. between 0 and 1.
+#' (required when dead compartments are included)
+#' @return This function returns a named numeric vector with diagonal values for the
+#' species in the food web (per unit time). It is important to review the units of the
+#' input data. If MR is biomass per unit time then BM must be just biomass. If MR is
+#' biomass per unit time per surface area then BM must be biomass per surface area.
+#' @export
+#' @examples
+getDiagonal <- function(MR, BM, dead = NULL, FM = NULL, AE = NULL) {
+
+  # Get diagonal, and find detritus values if necessary
+  if(!is.null(dead)) {
+    diagonal <- diag(FM)
+    add <- getDiagonalDetritus(FM = FM, BM = BM, AE = AE, dead = dead)
+  } else {
+    diagonal <- MR
+  }
+
+  # Find species values and add to diagonal
+  aii <- getDiagonalSpecies(MR = MR, BM = BM)
+  diagonal[names(aii)] <- aii
+  # Add detritus values to diagonal if necessary
+  if(!is.null(dead)) {
+    diagonal[names(add)] <- add
+  }
+
+  return(diagonal)
+}
+
--- tests/testthat/test_get_diagonal.R
+++ tests/testthat/test_get_diagonal.R
@@ -3,12 +3,13 @@ context("Diagonal value calculation")
 #### getSpeciesDiagonal
 
 # Proper data
-MR <- c(10,20,30)
-BM <- c(3,2,1)
+MR <- c(15,10,5) ; names(MR) <- c("PLANT", "WORM", "ANT")
+BM <- c(30, 20, 10) ; names(BM) <- c("PLANT", "WORM", "ANT")
+result <- -MR/BM ; names(result) <- c("PLANT", "WORM", "ANT")
 
 test_that("getDiagonalSpecies works with correct input data", {
   expect_equal(getDiagonalSpecies(MR = MR, BM = BM),
-               -MR/BM)
+               result)
 })
 
 # Error: the vector lengths are unequal
@@ -27,15 +28,60 @@ test_that("getDiagonalSpecies needs numeric input vectors", {
                "input vectors must be numeric")
 })
 
+# Error: the vectors are unnamed
+MR1 <- MR ; names(MR1) <- NULL
+BM1 <- BM ; names(BM1) <- NULL
+test_that("getDiagonalSpecies only executes with named vectors", {
+  expect_error(getDiagonalSpecies(MR = MR1, BM = BM),
+               "input vectors must be named")
+  expect_error(getDiagonalSpecies(MR = MR, BM = BM1),
+               "input vectors must be named")
+})
+
+# Error: the names do not match or in the wrong order
+MR2 <- MR ; names(MR2) <- names(MR)[c(2,3,1)]
+BM2 <- BM; names(BM2) <- c("a", "b", "c")
+test_that("getDiagonalSpecies only executes with matching names of vectors", {
+  expect_error(getDiagonalSpecies(MR = MR2, BM = BM),
+               "names of vectors do not match")
+  expect_error(getDiagonalSpecies(MR = MR, BM = BM2),
+               "names of vectors do not match")
+})
+
 
 #### getDetritusDiagonal
 
 # Proper data
-CR <- c(10,20,30)
-AE <- c(0.3, 0.2, 0.1)
-BM <- 25
+FM <- matrix(c(0, 3, 8, 7, 0, 0, 4, 4, 0), nrow = 3, ncol = 3)
+rownames(FM) <- c("DETRITUS", "PLANT", "ANIMAL")
+colnames(FM) <- c("DETRITUS", "PLANT", "ANIMAL")
+BM <- c(30, 20, 10) ; names(BM) <- c("DETRITUS", "PLANT", "ANIMAL")
+AE <- c(NA, 0.2, 0.3) ; names(AE) <- c("DETRITUS", "PLANT", "ANIMAL")
+GE <- c(NA, 0.2, 0.3) ; names(GE) <- c("DETRITUS", "PLANT", "ANIMAL")
+result <- -1/BM[1] * (AE[2] * FM[1,2] + AE[3] * FM[1,3])
 
 test_that("getDiagonalDetritus works with correct data input", {
-  expect_equal(getDiagonalDetritus(CR = CR, AE = AE, BM = BM),
-               -sum(AE * CR) / BM)
+  expect_equal(getDiagonalDetritus(FM = FM, BM = BM, AE = AE, dead = "DETRITUS"),
+               result)
+})
+
+
+
+#### getDiagonal
+# Proper data
+MR <- c(NA, 10, 5) ; names(MR) <- c("DETRITUS", "PLANT", "ANIMAL")
+FM <- matrix(c(0, 3, 8, 7, 0, 0, 4, 4, 0), nrow = 3, ncol = 3)
+rownames(FM) <- c("DETRITUS", "PLANT", "ANIMAL")
+colnames(FM) <- c("DETRITUS", "PLANT", "ANIMAL")
+BM <- c(30, 20, 10) ; names(BM) <- c("DETRITUS", "PLANT", "ANIMAL")
+AE <- c(NA, 0.2, 0.3) ; names(AE) <- c("DETRITUS", "PLANT", "ANIMAL")
+GE <- c(NA, 0.2, 0.3) ; names(GE) <- c("DETRITUS", "PLANT", "ANIMAL")
+result <- c(-1/BM[1] * (AE[2] * FM[1,2] + AE[3] * FM[1,3]),
+            -MR[2] / BM[2],
+            -MR[3] / BM[3]
+            )
+
+test_that("getDiagonal works with correct data input", {
+  expect_equal(getDiagonal(MR = MR, FM = FM, BM = BM, AE = AE, dead = "DETRITUS"),
+               result)
 })
