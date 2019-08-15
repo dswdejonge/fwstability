69c30d8eaab00c809844bc1a9eb852f0f3c23a51
dswdejonge <dsw.dejonge@gmail.com>
2019-08-15 19:27:50 +0200

    Change test because fraction is not input anymore.

--- tests/testthat/test_adjust_dead.R
+++ tests/testthat/test_adjust_dead.R
@@ -1,10 +1,9 @@
 context("Adjust the list \"dead\" input")
 
 
-answer1 <- list(names = "DET", def = NULL, frac = NULL)
-answer2 <- list(names = c("DET","NUT"), def = NULL, frac = NULL)
-answer3 <- list(names = c("DET", "NUT"), def = c("Def", "noDef"), frac = NULL)
-answer4 <- list(names = c("DET", "NUT"), frac = c(0.5, 0.5), def = NULL)
+answer1 <- list(names = "DET", def = NULL)
+answer2 <- list(names = c("DET","NUT"), def = NULL)
+answer3 <- list(names = c("DET", "NUT"), def = c("Def", "noDef"))
 
 test_that("the list \"dead\" gets adjusted correctly", {
   expect_equal(adjustDeadInput("DET"), answer1)
@@ -12,6 +11,5 @@ test_that("the list \"dead\" gets adjusted correctly", {
   expect_equal(adjustDeadInput(c("DET","NUT")), answer2)
   expect_equal(adjustDeadInput(list(c("DET", "NUT"))), answer2)
   expect_equal(adjustDeadInput(list(c("DET", "NUT"), c("Def", "noDef"))), answer3)
-  expect_equal(adjustDeadInput(list(c("DET", "NUT"), c(0.5, 0.5))), answer4)
 })
 
