d22aa1397aabb6da3c900647f0a06e8883d2e98c
dswdejonge <dsw.dejonge@gmail.com>
2019-08-27 17:25:46 +0200

    Add TODO list for taking complete solved lim model as input.

---
+++ tests/testthat/test_lim_input.R
@@ -0,0 +1,23 @@
+# Automatically include lim model into the getJacobian matrix.
+# Format needs to be so that the function can automatically find:
+# - flow matrix
+# - biomasses
+# - assimilation efficiencies
+# - growth efficiencies
+# - mortality rates
+# - dead compartments
+# - defecation flows
+# - parallel flows to defecation into the same compartment
+# - externals
+# - getXratioMatrix
+
+# Import lim model
+
+# solve lim model
+
+# use solved lim model in getJacobian
+# write tests for all individual functions (getFlowMatrix, getAEmatrix, etc.)
+# getFM has bug.
+
+
+# test against expected outcome.
