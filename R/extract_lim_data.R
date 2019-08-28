f2c6858092fc1e287bbfebbec929ec5bba793383
dswdejonge <dsw.dejonge@gmail.com>
2019-08-28 17:14:02 +0200

    Adjust Flowmatrix function from LIM package. Enhancement test lim and test.

--- R/extract_lim_data.R
+++ R/extract_lim_data.R
@@ -3,53 +3,41 @@
 #' This function extracts flow data from the LIM and produces a Flowmatrix i.e. flows from sources
 #' in rows to the sinks in columns.
 #' @references LIM package reference
-#' @param lim (required) A lim model.
-#' @details The LIM must be set up in a specific way.
+#' @param readLIM (required) A linear inverse model read into the R environment with the R-package LIM.
+#' Can be achieved through \code{readLIM <- Read("path_to_input_file")}
+#' @param web (optional) The solved (food) web problem, i.e. the values of the unknowns;
+#' if not specified, the model is solved first, using Ldei.
+#' @details The LIM must be set up in a specific way. sum columns must equal sum rows.
 #' @return Returns an energy-flux model in the format needed for the getJacobian function.
 #' @export
-getFlowMatrix <- function (originalFM = NULL, flows = NULL, answers = NULL) {
-  # Check function input
-  if(is.null(originalFM)) {stop("Supply flowmatrix with flownumbers from LIM$Flowmatrix")}
-  if(is.null(flows)) {stop("Supply the flows dataframe from readLIM$flows")}
-  if(is.null(answers)) {stop("Supply a vector with all flow values")}
-
-  # Create a vector which will store all total flow values
-  totalflows <- numeric(length(flows[,1]))
-  # Create a vector with all flownumbers
-  flownrs <- c(1:length(flows[,1]))
-
-  for(flownr in flownrs) {
-    same <- findParallelFlows(flows = flows, flownr = flownr)
-    totalflow <- sum(answers[same], na.rm = TRUE) # sum the flow values of all flows with the same source and sink.
-    totalflows[flownr] <- totalflow # store the value at the index of the original flow
-  }
-
-  # Find the locations in the matrix with flownumbers
-  indices <- match(flownrs, originalFM)
-  indices <- indices[!is.na(indices)] # remove NA's (no matches) from the list.
-  # Replace the flownumbers in the original matrix with the corresponding
-  # total flow values (so if the flownr is 3 in the matrix, but it should be flow
-  # 2 and 3, it will insert the totalflow of flow 2 and 3).
-  FM <- replace(originalFM, indices, totalflows[originalFM[indices]])
-  # Return a flowmatrix with flow values.
-  return(FM)
-}
-
-function (lim, web = NULL)
-{
+getFlowMatrix <- function(readLIM, web = NULL) {
+  lim <- Setup(readLIM)
+  flows <- readLIM$flows[,1:2]
   flowmatrix <- lim$Flowmatrix
-  if (is.null(web))
-    if (!is.null(lim$Cost) || !is.null(lim$Profit))
+  if(is.null(web)) {
+    if(!is.null(lim$Cost) || !is.null(lim$Profit)) {
       web <- Linp(lim)$X
-    else web <- Lsei.lim(lim, parsimonious = TRUE)$X
-    X <- as.vector(web)
-    Xpos <- pmax(0, X)
-    ii <- which(flowmatrix > 0, arr.ind = TRUE)
-    flowmatrix[ii] <- Xpos[lim$Flowmatrix[ii]]
-    Xneg <- -1 * pmin(0, X)
-    if (sum(Xneg) > 0)
-      flowmatrix[ii[, c(2, 1)]] <- flowmatrix[ii[, c(2, 1)]] +
-      Xneg[lim$Flowmatrix[ii]]
-    return(flowmatrix)
+    } else {
+      web <- Lsei(lim, parsimonious = TRUE)$X
+    }
+  }
+  X <- as.vector(web)
+  dup <- which(duplicated(flows))
+  for(i in dup) {
+    a <- which(flows[,1] == flows[i, 1])
+    b <- which(flows[,2] == flows[i, 2])
+    same <- c(a, b)[duplicated(c(a, b))]
+    X[i] <- sum(X[same])
+  }
+  Xpos <- pmax(0, X)
+  ii <- which(flowmatrix > 0, arr.ind = TRUE)
+  flowmatrix[ii] <- Xpos[lim$Flowmatrix[ii]]
+  Xneg <- -1 * pmin(0, X)
+  if(sum(Xneg) > 0) {
+    flowmatrix[ii[, c(2, 1)]] <- flowmatrix[ii[, c(2, 1)]] +
+    Xneg[lim$Flowmatrix[ii]]
+  }
+  return(flowmatrix)
 }
-<environment: namespace:LIM>
+
+
--- tests/testthat/test_lim_input.R
+++ tests/testthat/test_lim_input.R
@@ -12,8 +12,82 @@
 # - getXratioMatrix
 
 # Import lim model
+library(LIM)
 
-# solve lim model
+# Solve lim model
+# readLIM allows you to easily access all data from the input file
+readLIM <- Read(system.file("extdata", "foodweb2.lim", package = "fwstability"))
+# Setup can directly use the input file, or the readLIM variable.
+# It transforms the model into a set of linear equations to be solved.
+lim <- Setup(readLIM)
+# The Least Distance optimization is used to quantify all unknown flows.
+lim_solved <- Ldei(lim)
+# Expected answer
+fwnames <- c("LABILE", "REFRAC", "MEIO", "MACRO", "IN", "OUT")
+nC = lim$NExternal + lim$NComponents
+FMe <- matrix(c(
+0,      0, 12.25, 0, 0, 0,
+0,      0,     0, 0, 0, 1.0375,
+4.9625, 1.0375,0, 5, 0, 1.25,
+4.4,    0,     0, 0, 0, 0.6,
+2.8875, 0,     0, 0, 0, 0,
+0,      0,     0, 0, 0, 0
+), nrow = nC, ncol = nC, byrow = TRUE)
+rownames(FMe) <- fwnames ; colnames(FMe) <- fwnames
+
+# Test
+test_that("the Flowmatrix function works with parallel flows", {
+  #expect_equal(Flowmatrix(lim, web = lim_solved$X), FMe)
+  expect_equal(getFlowMatrix(readLIM), FMe)
+})
+
+
+
+
+
+
+
+#BM <- c(30, 20, 10, 5) ; names(BM) <- fwnames
+#AE <- c(NA, NA, 0.3, 0.3) ; names(AE) <- fwnames
+#GE <- c(NA, NA, 0.3, 0.3) ; names(GE) <- fwnames
+#DM <- matrix(1, nrow = 4, ncol = 4)
+#rownames(DM) <- fwnames
+#colnames(DM) <- fwnames
+#DM["MEIO", "LABILE"] <- 2/3
+#DM["MACRO", "LABILE"] <- 1/2
+#FDM <- FM * DM
+#dead <- list(names = c("LABILE", "REFRAC"), def = c("Def", "Def"), frac = DM)
+#model <- list(
+#  type = "EF", FM = FM, BM = BM, AE = AE, GE = GE, dead = dead
+#)
+## Expected answer
+#JM <- matrix(c(0,
+ #              0,
+#               (FM[3,1] - FM[1,3] + FM[3,4]*(1-AE[4])*(FDM[4,1]/(FDM[4,1]+FDM[4,2]))) / BM[3],
+#               (FM[4,1] - FM[1,4]) / BM[4],#
+#
+#               0,
+#               0,
+#               (FM[3,2] - FM[2,3] + FM[3,4]*(1-AE[4])*(FDM[4,2]/(FDM[4,1]+FDM[4,2]))) / BM[3],
+#               (FM[4,2] - FM[2,4]) / BM[4],
+#
+ #              AE[3] * GE[3] * FM[1,3] / BM[1],
+#               AE[3] * GE[3] * FM[2,3] / BM[2],
+#               0,
+#               -FM[3,4] / BM[4],
+
+ #              AE[4] * GE[4] * FM[1,4] / BM[1],
+#               AE[4] * GE[4] * FM[2,4] / BM[2],
+#               AE[4] * GE[4] * FM[3,4] / BM[3],
+#               0
+#), nrow = 4, ncol = 4)
+#rownames(JM) <- fwnames ; colnames(JM) <- fwnames
+
+#test_that("the function works with defecation and mortality into same compartment", {
+#  expect_equal(getJacobian(model),
+ #              JM)
+#})
+
 
 # use solved lim model in getJacobian
 # write tests for all individual functions (getFlowMatrix, getAEmatrix, etc.)
