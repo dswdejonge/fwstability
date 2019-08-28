038f69bf4803f5985ecd26a576f66cf4541e1f07
dswdejonge <dsw.dejonge@gmail.com>
2019-08-28 11:59:52 +0200

    Added original getFM function from LIM package to compare.

--- R/extract_lim_data.R
+++ R/extract_lim_data.R
@@ -34,3 +34,22 @@ getFlowMatrix <- function (originalFM = NULL, flows = NULL, answers = NULL) {
   # Return a flowmatrix with flow values.
   return(FM)
 }
+
+function (lim, web = NULL)
+{
+  flowmatrix <- lim$Flowmatrix
+  if (is.null(web))
+    if (!is.null(lim$Cost) || !is.null(lim$Profit))
+      web <- Linp(lim)$X
+    else web <- Lsei.lim(lim, parsimonious = TRUE)$X
+    X <- as.vector(web)
+    Xpos <- pmax(0, X)
+    ii <- which(flowmatrix > 0, arr.ind = TRUE)
+    flowmatrix[ii] <- Xpos[lim$Flowmatrix[ii]]
+    Xneg <- -1 * pmin(0, X)
+    if (sum(Xneg) > 0)
+      flowmatrix[ii[, c(2, 1)]] <- flowmatrix[ii[, c(2, 1)]] +
+      Xneg[lim$Flowmatrix[ii]]
+    return(flowmatrix)
+}
+<environment: namespace:LIM>
