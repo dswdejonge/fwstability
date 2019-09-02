5df64e3477d85675e19de2b919255ccc52d91388
dswdejonge <dsw.dejonge@gmail.com>
2019-09-02 14:19:04 +0200

    Finish getVariables function.

--- R/get_variables.R
+++ R/get_variables.R
@@ -9,12 +9,12 @@
 #' @details The optimal solution of the LIM only provides the flow solutions.
 #' However, in the LIM variables can be defined as the sum of some values.
 #' This function calculates the actual value of these variables based on the flow solutions.
-#' @return Returns an energy-flux model in the format needed for the getJacobian function.
+#' @return Returns a named vector with all variable values.
 #' @export
-getsVariables <- function(readLIM, web = NULL) {
+getVariables <- function(readLIM, web = NULL) {
   lim <- Setup(readLIM)
-  vars <- numeric(lim$NVariables) # vals
-  pars <- readLIM$pars$val # parvec
+  vars <- numeric(lim$NVariables)
+  pars <- readLIM$pars$val
   vareq <- readLIM$vars
   if(is.null(web)) {
     if(!is.null(lim$Cost) || !is.null(lim$Profit)) {
@@ -24,27 +24,18 @@ getsVariables <- function(readLIM, web = NULL) {
     }
   }
   # Possible to remove loop?
-  for (i in 1:LIM$NVariables) {
-    # refresh vector with variable parsimonious values
-    varvec <- vals # vector with all variables values in right order: needs to be refreshed every time
-
-    # get subset with the same equation nr
+  for (i in 1:lim$NVariables) {
+    tempvars <- vars
     subset <- vareq[vareq$nr == i,]
-
-    # takes parameter, variable or flow nr from subset to use as index
-    # to find the corresponding parsimonious values and
-    # multiplies them by 'val' which is the coefficient (like 1 or -1) and sums them
     sum <-
-      sum(parvec[subset$par1]*subset$val, na.rm = TRUE) +
-      sum(parvec[subset$par2]*subset$val, na.rm = TRUE) +
-      sum(parvec[subset$par3]*subset$val, na.rm = TRUE) +
-      sum(parvec[subset$par4]*subset$val, na.rm = TRUE) +
-      sum(varvec[subset$var]*subset$val, na.rm = TRUE) +
-      sum(flowvec[subset$flow]*subset$val, na.rm = TRUE)
-
-    # replaces 0 in data frame to actual sum value
-    vals[i] = sum
+      sum(pars[subset$par1]*subset$val, na.rm = TRUE) +
+      sum(pars[subset$par2]*subset$val, na.rm = TRUE) +
+      sum(pars[subset$par3]*subset$val, na.rm = TRUE) +
+      sum(pars[subset$par4]*subset$val, na.rm = TRUE) +
+      sum(tempvars[subset$var]*subset$val, na.rm = TRUE) +
+      sum(web[subset$flow]*subset$val, na.rm = TRUE)
+    vars[i] = sum
   }
-  vals <- data.frame(LIM$Variables, vals)
-  return(vals)
+  names(vars) <- lim$Variables
+  return(vars)
 }
