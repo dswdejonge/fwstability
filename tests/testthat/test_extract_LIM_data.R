7655eb9af10b4f0223e0c17a485e5577664312bd
dswdejonge <dsw.dejonge@gmail.com>
2019-09-03 12:00:41 +0200

    Update test for extractLIMdata to include JM.

--- tests/testthat/test_extract_LIM_data.R
+++ tests/testthat/test_extract_LIM_data.R
@@ -1,29 +1,22 @@
-# Automatically include lim model into the getJacobian matrix.
-# Format needs to be so that the function can automatically find:
-# - flow matrix
-# - biomasses
-# - assimilation efficiencies
-# - growth efficiencies
-# - mortality rates
-# - dead compartments
-# - defecation flows
-# - parallel flows to defecation into the same compartment
-# - externals
-# - getXratioMatrix
+context("Finding the right answer of a LIM")
 
+# input
 library(LIM)
-# readLIM allows you to easily access all data from the input file
 readLIM <- Read(system.file("extdata", "foodweb2.lim", package = "fwstability"))
-# Setup can directly use the input file, or the readLIM variable.
-# It transforms the model into a set of linear equations to be solved.
-lim <- Setup(readLIM)
-# The Least Distance optimization is used to quantify all unknown flows.
-lim_solved <- Ldei(lim)
+model <- list(
+  type = "LIM",
+  LIM = readLIM,
+  dead = list(names = c("LABILE", "REFRAC"))
+)
+
 
 # Expected answers
+lim <- Setup(readLIM)
+lim_solved <- Ldei(lim)
+
 fwnames <- c("LABILE", "REFRAC", "MEIO", "MACRO", "IN", "OUT")
 nC = lim$NExternal + lim$NComponents
-FMe <- matrix(c(
+FM <- matrix(c(
 0,      0, 12.25, 0, 0, 0,
 0,      0,     0, 0, 0, 1.0375,
 4.9625, 1.0375,0, 5, 0, 1.25,
@@ -31,7 +24,8 @@ FMe <- matrix(c(
 2.8875, 0,     0, 0, 0, 0,
 0,      0,     0, 0, 0, 0
 ), nrow = nC, ncol = nC, byrow = TRUE)
-rownames(FMe) <- fwnames ; colnames(FMe) <- fwnames
+rownames(FM) <- fwnames ; colnames(FM) <- fwnames
+
 variables <- c(lim_solved$X["meioDefLab"] + lim_solved$X["meioDefRefrac"],
                lim_solved$X["macroDefLab"] + lim_solved$X["macroDefRefrac"],
                lim_solved$X["meioGrazDet"] - lim_solved$X["meioDefLab"] - lim_solved$X["meioDefRefrac"],
@@ -40,9 +34,10 @@ variables <- c(lim_solved$X["meioDefLab"] + lim_solved$X["meioDefRefrac"],
                lim_solved$X["macroPredMeio"] - lim_solved$X["macroDefLab"] - lim_solved$X["macroDefRefrac"] - lim_solved$X["macroResp"]
                )
 names(variables) <- lim$Variables
+
 AE <- c(NA, NA,
-        variables["meioAss"]/sum(FMe[,"MEIO"], na.rm = T),
-        variables["macroAss"]/sum(FMe[,"MACRO"], na.rm = T)
+        variables["meioAss"]/sum(FM[,"MEIO"], na.rm = T),
+        variables["macroAss"]/sum(FM[,"MACRO"], na.rm = T)
         )
 GE <- c(NA, NA,
         variables["meioGrowth"]/variables["meioAss"],
@@ -50,11 +45,43 @@ GE <- c(NA, NA,
         )
 names(AE) <- lim$Components$name ; names(GE) <- lim$Components$name
 
+BM <- c(30, 20, 10, 5) ; names(BM) <- fwnames[1:4]
+DM <- matrix(1, nrow = 4, ncol = 4)
+rownames(DM) <- fwnames[1:4]
+colnames(DM) <- fwnames[1:4]
+DM["MEIO", "LABILE"] <- lim_solved$X["meioDefLab"] / FM["MEIO", "LABILE"]
+DM["MACRO", "LABILE"] <- lim_solved$X["macroDefLab"] / FM["MACRO", "LABILE"]
+FDM <- FM[1:4,1:4] * DM
+
+JM <- matrix(c(0,
+              0,
+               (FM[3,1] - FM[1,3] + FM[3,4]*(1-AE[4])*(FDM[4,1]/(FDM[4,1]+FDM[4,2]))) / BM[3],
+               (FM[4,1] - FM[1,4]) / BM[4],#
+
+               0,
+               0,
+               (FM[3,2] - FM[2,3] + FM[3,4]*(1-AE[4])*(FDM[4,2]/(FDM[4,1]+FDM[4,2]))) / BM[3],
+               (FM[4,2] - FM[2,4]) / BM[4],
+
+               AE[3] * GE[3] * FM[1,3] / BM[1],
+               AE[3] * GE[3] * FM[2,3] / BM[2],
+               0,
+               -FM[3,4] / BM[4],
+
+               AE[4] * GE[4] * FM[1,4] / BM[1],
+               AE[4] * GE[4] * FM[2,4] / BM[2],
+               AE[4] * GE[4] * FM[3,4] / BM[3],
+               0
+), nrow = 4, ncol = 4)
+rownames(JM) <- fwnames[1:4] ; colnames(JM) <- fwnames[1:4]
+
+
+
 # Tests
 test_that("the Flowmatrix function works with parallel flows", {
-  #expect_equal(Flowmatrix(lim, web = lim_solved$X), FMe)
-  expect_equal(getFlowMatrix(readLIM), FMe)
-  expect_equal(getFlowMatrix(readLIM, web = lim_solved$X), FMe)
+  #expect_equal(Flowmatrix(lim, web = lim_solved$X), FM)
+  expect_equal(getFlowMatrix(readLIM), FM)
+  expect_equal(getFlowMatrix(readLIM, web = lim_solved$X), FM)
 })
 
 test_that("the getVariables function gives right answer", {
@@ -62,61 +89,13 @@ test_that("the getVariables function gives right answer", {
 })
 
 test_that("the right conversion efficiencies are extracted", {
-  expect_equal(getCE(FM = FMe, vars = variables, lim = lim, aTag = "ass", gTag = "growth")$AE,
+  expect_equal(getCE(FM = FM, vars = variables, lim = lim, aTag = "ass", gTag = "growth")$AE,
                AE)
-  expect_equal(getCE(FM = FMe, vars = variables, lim = lim, aTag = "ass", gTag = "growth")$GE,
+  expect_equal(getCE(FM = FM, vars = variables, lim = lim, aTag = "ass", gTag = "growth")$GE,
                GE)
 })
 
+test_that("the right Jacobian matrix is produced from the LIM", {
+  expect_equal(getJacobian(model), JM)
+})
 
-
-
-
-
-
-#BM <- c(30, 20, 10, 5) ; names(BM) <- fwnames
-#DM <- matrix(1, nrow = 4, ncol = 4)
-#rownames(DM) <- fwnames
-#colnames(DM) <- fwnames
-#DM["MEIO", "LABILE"] <- 2/3
-#DM["MACRO", "LABILE"] <- 1/2
-#FDM <- FM * DM
-#dead <- list(names = c("LABILE", "REFRAC"), def = c("Def", "Def"), frac = DM)
-#model <- list(
-#  type = "EF", FM = FM, BM = BM, AE = AE, GE = GE, dead = dead
-#)
-## Expected answer
-#JM <- matrix(c(0,
- #              0,
-#               (FM[3,1] - FM[1,3] + FM[3,4]*(1-AE[4])*(FDM[4,1]/(FDM[4,1]+FDM[4,2]))) / BM[3],
-#               (FM[4,1] - FM[1,4]) / BM[4],#
-#
-#               0,
-#               0,
-#               (FM[3,2] - FM[2,3] + FM[3,4]*(1-AE[4])*(FDM[4,2]/(FDM[4,1]+FDM[4,2]))) / BM[3],
-#               (FM[4,2] - FM[2,4]) / BM[4],
-#
- #              AE[3] * GE[3] * FM[1,3] / BM[1],
-#               AE[3] * GE[3] * FM[2,3] / BM[2],
-#               0,
-#               -FM[3,4] / BM[4],
-
- #              AE[4] * GE[4] * FM[1,4] / BM[1],
-#               AE[4] * GE[4] * FM[2,4] / BM[2],
-#               AE[4] * GE[4] * FM[3,4] / BM[3],
-#               0
-#), nrow = 4, ncol = 4)
-#rownames(JM) <- fwnames ; colnames(JM) <- fwnames
-
-#test_that("the function works with defecation and mortality into same compartment", {
-#  expect_equal(getJacobian(model),
- #              JM)
-#})
-
-
-# use solved lim model in getJacobian
-# write tests for all individual functions (getFlowMatrix, getAEmatrix, etc.)
-# getFM has bug.
-
-
-# test against expected outcome.
