1ef49553416f35462b8d4fbab28cc9c0318d8602
dswdejonge <dsw.dejonge@gmail.com>
2019-09-24 14:57:43 +0200

    Add model with reciprocal predation and test.

--- tests/testthat/test_get_jacobian.R
+++ tests/testthat/test_get_jacobian.R
@@ -2,40 +2,37 @@ context("Jacobian matrix creation")
 
 source("models/model_regular.R")
 test_that("the function works without optional arguments", {
-  expect_equal(getJacobian(model),
-               JM)
+  expect_equal(getJacobian(model), JM)
+  # netto FM
+  expect_equal(getJacobian(model1), JM1)
 })
 
 source("models/model_dead.R")
 test_that("correct JM created with dead compartments", {
-  expect_equal(getJacobian(model),
-               JM)
+  expect_equal(getJacobian(model), JM)
+  # netto FM
+  expect_equal(getJacobian(model4), JM4)
 })
 test_that("getJacobian incorporates vector diagonal correctly", {
-  expect_equal(getJacobian(model2),
-               JM2)
+  expect_equal(getJacobian(model2), JM2)
 })
 test_that("getJacobian calculates the diagonal from flux values", {
-  expect_equal(getJacobian(model3),
-               JM3)
+  expect_equal(getJacobian(model3), JM3)
 })
 
 source("models/model_externals.R")
 test_that("correct JM created with external compartments", {
-  expect_equal(getJacobian(model),
-               JM)
+  expect_equal(getJacobian(model), JM)
 })
 
 source("models/model_dead_externals.R")
 test_that("correct JM created with both dead and external arguments", {
-  expect_equal(getJacobian(model),
-               JM)
+  expect_equal(getJacobian(model), JM)
 })
 
 source("models/model_multiple_dead.R")
 test_that("correct JM created with multiple dead compartments but one defecation", {
-  expect_equal(getJacobian(model),
-               JM)
+  expect_equal(getJacobian(model), JM)
 })
 
 source("models/model_mort_def_same_comp.R")
@@ -46,17 +43,20 @@ test_that("correct JM created with defecation and mortality into same compartmen
 
 source("models/model_multiple_def.R")
 test_that("correct JM created with multiple dead and defecation compartments", {
-  expect_equal(getJacobian(model),
-               JM)
+  expect_equal(getJacobian(model), JM)
 })
 
 source("models/model_deRuiter1995.R")
 test_that("correct JM created with data from De Ruiter et al. 1995", {
-  expect_equal(getJacobian(model),
-               JM)
+  expect_equal(getJacobian(model), JM)
 })
 
 source("models/model_Lotka_Volterra.R")
 test_that("The package works with a dynamic LV function", {
   expect_equal(getStability(getJacobian(model)), answer)
+})
+
+source("models/model_Omni_Pred.R")
+test_that("correct JM created with reciprocal predation and detritus", {
+  expect_equal(getJacobian(model), JM)
 })
