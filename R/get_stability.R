c47bfbf42d025068f08bf71a8eac1c90e6808b0e
dswdejonge <dsw.dejonge@gmail.com>
2020-02-17 15:32:27 +0100

    Add initial stability to the getStability function.

--- R/get_stability.R
+++ R/get_stability.R
@@ -30,7 +30,7 @@ getScalarStability <- function(JM, MR, stepsize, to_scale) {
 getInitialStability <- function(JM) {
   # Arnoldi, J.F., Loreau, M., Haegeman, B., 2016.
   # Resilience, reactivity and variability: A mathematical comparison
-  #of ecological stability measures.
+  # of ecological stability measures.
   # J. Theor. Biol. 389, 47–59.
   # https://doi.org/10.1016/j.jtbi.2015.10.012
   stability <- 0.5*getMaxReEV(JM + t(JM))
@@ -42,7 +42,7 @@ getInitialStability <- function(JM) {
 #' @details
 #' Initial stability is the initial reaction of a system to a small perturbation.
 #' Initial stability is found by equation 4 from Arnoldi et al. (2016),
-#' with the difference that this function does not change the sign of the value.
+#' with the difference that th function in this package does not change the sign of the value.
 #' Therefore, a negative initial stability means the system is reactive i.e. the perturbations are amplified.
 #' \cr
 #' \cr
@@ -59,7 +59,8 @@ getInitialStability <- function(JM) {
 #' \code{getJacobian}
 #' \cr
 #' \cr
-#' The "scalar" method relies on the estimation of natural mortality rates (t-1).
+#' The "scalar" method for asymptotic stability relies on the estimation of natural mortality rates
+#' (unit per time, t-1).
 #' A critical matrix, i.e. a matrix that is on the stability threshold, is calculated by setting the
 #' diagonal to the given mortality values and subtracting the maximum real part of the eigenvalues
 #' from each diagonal value.
@@ -69,13 +70,14 @@ getInitialStability <- function(JM) {
 #' stepsize until the matrix becomes stable, i.e. the maximum real part of the eigenvalues
 #' is negative.
 #' If dead compartments exist, their diagonal values are not scaled, because dead compartments do not
-#' have mortality rates. Therefore, the diagonal values of dead compartments in the Jacobian matrix should
-#' be quantified correctly, or set to zero (assuming there is no intracompartmental feedback).
+#' have natural mortality rates. Therefore, the diagonal values of dead compartments in the Jacobian matrix should
+#' be quantified correctly, or set to zero (assuming there is no intracompartmental detritus feedback).
 #' \cr \cr
 #' Mortality rates per unit time can for example be calculated as the mortality flux (biomass
-#' per surface area per unit time) divided by the biomass (biomass) per surface area, or as the
-#' inverse of the natural lifespan of the species. The mortality flux can be found with the
-#' function \code{getMortalityRates}, which assumes natural mortality equal production
+#' per surface area per unit time) divided by the biomass per surface area, or as the
+#' inverse of the natural lifespan of the species. Mortality fluxes can be found with the
+#' function \code{getMortalityRates} if you have the relevant data;
+#' the function assumes natural mortality equals production
 #' (AE x GE x Consumption) minus predation (flux to all other faunal compartments).
 #' @param JM (required) A square named Jacobian matrix with numeric values representing the effect of one compartment (rows)
 #' on another compartment (columns).
@@ -90,8 +92,9 @@ getInitialStability <- function(JM) {
 #' rates needed to acquire a stable matrix.
 #' }
 #' \item{
-#' The "initial" method finds initial stability as equation 4 from Arnoldi et al. (2016), based on half the the maximum
-#' real part of the eigenvalues from the addition of the Jacobian matrix to its transpose.
+#' The "initial" method finds initial stability as equation 4 from Arnoldi et al. (2016);
+#' initial stability is half the maximum real part of the eigenvalues derived from the
+#' matrix obtained by addition of the Jacobian matrix to its transpose.
 #' }
 #' }
 #' @param MR (required if method is "scalar")
@@ -120,7 +123,7 @@ getInitialStability <- function(JM) {
 #' getStability(JM)
 getStability <- function(JM, method = "eigenvalue",
                          MR = NULL, dead_names = NULL) {
-  # Check data format
+  # Errors: check data format
   checkMformat(JM)
   checkNamingFormat(matrices = list(JM), vectors = list(MR))
   checkStabilityMethod(method, JM, MR)
@@ -131,20 +134,23 @@ getStability <- function(JM, method = "eigenvalue",
   # Warnings
   if(method == "eigenvalue" && (!is.null(MR) | !is.null(dead_names))) {
     warning("given mortality values or dead compartments are irrelevant for the eigenvalue method")
-  }
-
-  # Get indices compartments to scale (excluding dead compartments)
-  to_scale <- 1:dim(JM)[1]
-  if(!is.null(dead_names)) {
-    to_scale <- to_scale[-which(rownames(JM) %in% dead_names)]
+  } else if(method == "initial" && (!is.null(MR) | !is.null(dead_names))) {
+    warning("given mortality values or dead compartments are irrelevant for the initial stability")
   }
 
   if(method == "eigenvalue") {
     stability <- getMaxReEV(JM)
   } else if (method == "scalar") {
+    # Get indices compartments to scale (excluding dead compartments)
+    to_scale <- 1:dim(JM)[1]
+    if(!is.null(dead_names)) {
+      to_scale <- to_scale[-which(rownames(JM) %in% dead_names)]
+    }
     diag(JM)[to_scale] <- -MR[to_scale]
     stepsize <- getStepSize(getCriticalDiagonal(JM), MR)
     stability <- getScalarStability(JM, MR, stepsize, to_scale)
+  } else if(method == "initial") {
+    stability <- getInitialStability(JM)
   }
   return(stability)
 }
