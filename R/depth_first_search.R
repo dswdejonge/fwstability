f0587aa00c10489e943d70d03b4bce84bbda8758
dswdejonge <dsw.dejonge@gmail.com>
2020-02-19 16:30:21 +0100

    Update dfs. Manual test.

--- R/depth_first_search.R
+++ R/depth_first_search.R
@@ -8,17 +8,17 @@
 #' @param pathway Integer vector that tracks the pathway to current node.
 #' @param started Logical vector which stores which nodes have been used
 #' before as starting node.
-#' @param output String with filename to store loops.
+#' @param file String with name for textfile that will store loops.
 #' @param start Is integer with the current start node.
 #' @references Based on the answers at: https://stackoverflow.com/questions/546655/finding-all-cycles-in-a-directed-graph
 #' @return No returned value, writes .txt file to working directory.
 #' @export
-dfsall <- function(AM, node, visited, pathway, started, output, start) {
+dfsall <- function(AM, node, visited, pathway, started, file, start) {
   if(visited[node]){
     if(node == start){
       cycle <- c(pathway, start)
       write(cycle,
-            file = output,
+            file = file,
             append = TRUE,
             ncolumns = length(cycle))
     }
@@ -28,7 +28,7 @@ dfsall <- function(AM, node, visited, pathway, started, output, start) {
   pathway <- c(pathway, node)
   children <- which(AM[node,] == 1)
   for(child in children){
-    dfsall(AM, child, visited, pathway, started, output, start)
+    dfsall(AM, node = child, visited, pathway, started, file, start)
   }
   visited[node] <- FALSE
   pathway[-length(pathway)]
@@ -45,12 +45,12 @@ dfsall <- function(AM, node, visited, pathway, started, output, start) {
 #' @param k Integer denoting the length of loops searched for.
 #' @param started Logical vector which stores which nodes have been used
 #' before as starting node.
-#' @param output String with filename to store loops.
+#' @param file String with filename to store loops.
 #' @param start Is integer with the current start node.
-#' #' @references Based on the answers at: https://stackoverflow.com/questions/546655/finding-all-cycles-in-a-directed-graph
+#' @references Based on the answers at: https://stackoverflow.com/questions/546655/finding-all-cycles-in-a-directed-graph
 #' @return No returned value, writes .txt file to working directory.
 #' @export
-dfsk <- function(AM, node, visited, pathway, k, started, output, start){
+dfsk <- function(AM, node, visited, pathway, k, started, file, start){
   if(started[node]){
     return()
   }
@@ -58,7 +58,7 @@ dfsk <- function(AM, node, visited, pathway, k, started, output, start){
     if(node == start && length(pathway) == k){
       cycle <- c(pathway, start)
       write(cycle,
-            file = output,
+            file = file,
             append = TRUE,
             ncolumns = length(cycle))
       return()
@@ -72,11 +72,11 @@ dfsk <- function(AM, node, visited, pathway, k, started, output, start){
     if(!(start %in% children)){
       return()
     }else{
-      dfsk(AM, start, visited, pathway, k, started, output, start)
+      dfsk(AM, node = start, visited, pathway, k, started, file, start)
     }
   }else{
     for(child in children){
-      dfsk(AM, child, visited, pathway, k, started, output, start)
+      dfsk(AM, node = child, visited, pathway, k, started, file, start)
     }
   }
   visited[node] <- FALSE
@@ -89,54 +89,61 @@ dfsk <- function(AM, node, visited, pathway, k, started, output, start){
 #' The wrapper checks data input, redirects to the right algorithm, and checks if no
 #' file will be overwritten.
 #' @param AM (required) Adjacency matrix
-#' @param k (optional) Integer of length loop to search for.
-#' @param output (optional) Filename to store loops. Default is "allLoops.txt"
+#' @param output (required) Name for textfile to store loops.
+#' The data will be stored as "<output>.txt" if all loops are searched for, or as
+#' "<output>_k=<k>.txt". Default is "allLoops", so the text file will be "allLoops.txt"
 #' or "allLoops_k=<k>.txt".
+#' @param k (optional) Default is NULL. Integer of length loop to search for.
 #' @param verbose (optional) Default is TRUE. Set to FALSE to hide messages.
-#' @references \itemize{
-#' \item{
-#' Reference?
-#' }
-#' }
 #' @return Returns file name.
+#' @references Based on the answers at: https://stackoverflow.com/questions/546655/finding-all-cycles-in-a-directed-graph
 #' @export
-dfs <- function(AM, k = NULL, output = NULL, verbose = T){
+dfs <- function(AM, output = "allLoops", k = NULL, verbose = T){
+  # Check input data
+  if(verbose){message("Checking input data.")}
   if(dim(AM)[1] != dim(AM)[2]){
     stop("Adjacency matrix must be square.")
   } else if(!all(AM == 1 | AM == 0)) {
     stop("Adjancency matrix can only contain 0 and 1.")
   } else if(!is.null(k) & !is.double(k)) {
     stop("k must be an integer.")
-  } else if(!is.null(output) & !is.character(output)) {
+  } else if(is.null(output) | !is.character(output)) {
     stop("output should contain string with filename for output.")
   }
 
+  # Initialize variables for dfs algorithm
   visited <- logical(length = dim(AM)[1])
   pathway <- numeric()
   started <- logical(length = dim(AM)[1])
 
-  if(is.null(k) & is.null(output)) {
-    output <- "allLoops.txt"
-  } else if(!is.null(k) & is.null(output)) {
-    output <- paste0("allLoops_k=",k,".txt")
+  # Determine file name
+  if(is.null(k)) {
+    file <- paste0(output,".txt")
+  } else {
+    file <- paste0(output,"_k=",k,".txt")
   }
-
-  if(file.exists(output)) {
-    stop(paste0("file ",output," already exists. Rename or remove to avoid overwriting."))
+  if(file.exists(file)) {
+    stop(paste0("The file ",file," already exists. Rename or remove to avoid overwriting."))
   } else if(verbose) {
-    message(paste0("Loops stored as ",output))
+    message(paste0("Loop output will be stored in ",file))
   }
 
   if(is.null(k)) {
+    if(verbose){message("Start dfs to find all loops.")}
     for(startnode in 1:dim(AM)[1]){
-      dfsall(AM, node = startnode, visited, pathway, started, output, start = startnode)
+      dfsall(AM, node = startnode,
+             visited = visited, pathway = pathway, started = started,
+             file = file, start = startnode)
       started[startnode] <- TRUE
     }
   } else {
+    if(verbose){message(paste0("Start dfs to find loops of length k=",k))}
     for(startnode in 1:dim(AM)[1]){
-      dfsk(AM, node = startnode, visited, pathway, k, started, output, start = startnode)
+      dfsk(AM, node = startnode,
+           visited = visited, pathway = pathway, k = k, started = started,
+           file = file, start = startnode)
       started[startnode] <- TRUE
     }
   }
-  return(output)
+  return(file)
 }
--- tests/testthat/test_depth_first_search.R
+++ tests/testthat/test_depth_first_search.R
@@ -13,28 +13,6 @@ AM <- matrix(c(
 ), byrow = T, nrow = 8, ncol = 8)
 AM1 <- AM ; AM1[3] <- 5
 
-#dfs(AM)
-#dfs(AM, k = 3)
-
-fwnames <- LETTERS[1:dim(AM)[1]]
-FM <- AM
-FM[which(FM == 1)] <- runif(length(which(FM == 1)), min = 0.01, max = 10)
-BM <- runif(dim(FM)[1], min = 5, max = 25)
-AE <- runif(dim(FM)[1], min = 0, max = 1)
-GE <- runif(dim(FM)[1], min = 0, max = 1)
-MR <- runif(dim(FM)[1], min = 0, max = 2)
-rownames(FM) <- fwnames ; colnames(FM) <- fwnames
-names(BM) <- fwnames ; names(AE) <- fwnames ; names(GE) <- fwnames
-model <- list(
-  type = "EF",
-  FM = FM,
-  BM = BM,
-  AE = AE,
-  GE = GE,
-  dead = list(names = "A", frac = FM)
-)
-JM <- getJacobian(model)
-
 test_that("the function only executes with right data input", {
   expect_error(dfs(AM[,1:4]), "Adjacency matrix must be square.")
   expect_error(dfs(AM1), "Adjancency matrix can only contain 0 and 1.")
@@ -42,15 +20,12 @@ test_that("the function only executes with right data input", {
   expect_error(dfs(AM, output = 3), "output should contain string with filename for output.")
   expect_error(dfs())
 })
-k <- 3
-IS <- JM[which(JM != 0)][1:k]
 
-test_that("getFeedback works", {
-  expect_equal(getFeedback(IS), prod(IS))
-})
-
-test_that("getLoopWeight works", {
-  expect_equal(getLoopWeight(IS), abs(prod(IS)) ^ (1 / k))
-  expect_equal(getLoopWeight(IS, MR), abs(prod(IS) / prod(MR)) ^ (1 / k))
-})
-
+# Manual test.
+# dfs(AM, k = 3, output = "tests/testthat/allLoops")
+#file <- "tests/testthat/allLoops_k=3.txt"
+#test_that("All loops of certain length are found", {
+#  expect_equal(
+#    sum(read.delim(file,sep = " ")[,1:3]),
+#    sum(1,1,2,2,2,3,4,4,7,7,6,6,7,4,8))
+#})
